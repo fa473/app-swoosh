4ffa9bc6c2e3b5160b2521526f1d40b03ca8407d
fa473 <fa473@users.noreply.github.com>
2017-12-11 17:50:12 -0800

    ui

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,18 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by home on 12/11/17.
+//  Copyright © 2017 home. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 2
+    }
+}
