5e0003c76fd1a062a2c7ab273dc2c6eaca3be132
fa473 <fa473@users.noreply.github.com>
2017-12-12 15:41:09 -0800

    created Player model

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by home on 12/12/17.
+//  Copyright © 2017 home. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+
+    
+}
